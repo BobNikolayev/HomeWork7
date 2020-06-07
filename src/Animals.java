bd9f692d7501b166db7a08a84e3745498b316178
MrNukeBlast <bnikolayev993@gmail.com>
2020-06-07 10:40:56 -0700

    hw7

--- src/Animals.java
+++ src/Animals.java
@@ -1,2 +1,32 @@
+import java.lang.reflect.Constructor;
+
 public class Animals {
+    int eat;
+    String name;
+    boolean feedUp;
+
+    public int getEat(){
+        return eat;
+    }
+
+    public boolean getFeegUp(){
+        return feedUp;
+    }
+
+
+    public boolean eat(Plate plate){
+        System.out.println(name + " eat...");
+        plate.foodDecrease(getEat());
+
+//проверка на сытость
+        if((plate.getFood() < getEat()) == false){
+            System.out.println("cat is feed up.");
+            return feedUp = true;
+        }
+        else{
+            System.out.println("cat is not feed up");
+            return feedUp = false;
+        }
+    }
+
 }
